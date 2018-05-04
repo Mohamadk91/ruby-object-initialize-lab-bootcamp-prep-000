55c90265a9beb239fe73b5e54b0422dc8b404b2f
Mohamad Khayat <moek91@outlook.com>
2018-05-04 01:14:34 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,12 +1,14 @@
-class Dog
- def initialize(name, breed="Mutt")
-   @name = name
-   @breed = breed
+class person  
+  def initialize(name)
+    @name = name
+  end
+  
+    def name=(name)
+    @name = name
+  end
 
- end
-
-
-
-
+  def name
+    @name
+  end
 
 end
