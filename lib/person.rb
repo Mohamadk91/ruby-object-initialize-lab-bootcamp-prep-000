67c1d7d480020c49a0d812ad5b352f8e5845c098
Mohamad Khayat <moek91@outlook.com>
2018-05-04 01:14:48 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,14 +1,14 @@
-class person  
+class person
   def initialize(name)
     @name = name
   end
   
-    def name=(name)
+  def name=(name)
     @name = name
   end
-
+  
   def name
     @name
   end
-
+  
 end
